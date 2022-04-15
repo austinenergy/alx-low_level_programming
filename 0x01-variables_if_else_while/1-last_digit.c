356bcf7b80739a9712698ee49635ad58e6503186
austinenergy <austinafia@gmail.com>
2022-04-15 09:34:30 -0700

    task9

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ else if (n % 10 == 0)
 {
 printf("Last digit of %i is %i and is 0\n", n, n % 10);
 }
-else 
+else if 
 {
 printf("Last digit of %i is %i and is less than 6 and not 0\n", n, n % 10);
 }
